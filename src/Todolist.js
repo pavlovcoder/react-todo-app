ad13c92e63804c32502e6472e75b16199451832e
Włodzimierz Pavlov <thepavlovcompany@gmail.com>
2018-05-06 22:15:15 +0200

    Add files via upload

---
+++ src/Todolist.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import TodoItems from './TodoItems';
+import './TodoList.css';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+    if (this._inputElement.value !== '') {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+      
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+
+      this._inputElement.value = '';
+    }
+    
+    console.log(this.state.items);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className='todoListMain'>
+        <h3 className='header-title'>Todo Application</h3>
+        <div className='header'>
+          <form onSubmit={this.addItem}>
+            <input  ref={(a) => this._inputElement = a} placeholder='Enter task'></input>
+            <button type='submit'>add</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items}/>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
