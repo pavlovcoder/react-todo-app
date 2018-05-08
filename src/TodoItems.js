8025cae5fa4794916a4cdac7e1f39f69e41bc987
Włodzimierz Pavlov <pavlovcoder@gmail.com>
2018-05-08 10:42:40 +0200

    Final commit
    Final commint

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import FlipMove from 'react-flip-move';
 
 class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
+  }
+
   createTasks(item) {
-    return <li key={item.key}>{item.text}</li>
+    return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+  }
+
+  delete(key) {
+    this.props.delete(key);
   }
 
   render() {
@@ -10,8 +20,10 @@ class TodoItems extends Component {
     var listItems = todoEntries.map(this.createTasks);
 
     return (
-      <ul className='theList'>
-        {listItems}
+      <ul className="theList">
+        <FlipMove duration={250} easing="ease-out">
+          {listItems}
+        </FlipMove>
       </ul>
     );
   }
